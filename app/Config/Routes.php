a07a0c396e06ce2df14b230b9c76128683e98e65
ry@n @chm@d <ryan30216@gmail.com>
2023-11-30 10:45:21 +0700

    nambah control page.php dan view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,8 +8,9 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 
 /**
- * authtentication and autozion routes
+ * rute untuk autentikasi 
  */
 $routes->match(['get', 'post'], 'register', 'UserAuth::registerpage');
 $routes->match(['get', 'post'], 'login', 'UserAuth::loginPage');
 $routes->get('logout', 'UserAuth::logout', ['filter' => 'auth']);
+$souter->get('buku', 'pages::buku',['fiterer' => 'auth']);
