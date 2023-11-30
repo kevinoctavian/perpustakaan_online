b7d72fc688754bd3b08a236cf8a2b42074a45266
kevinoctavian <kebimbangan12@gmail.com>
2023-11-30 12:48:04 +0700

    Memperbaiki authentikasi

--- app/Controllers/UserAuth.php
+++ app/Controllers/UserAuth.php
@@ -7,108 +7,120 @@ use App\Models\UsersModel;
 
 class UserAuth extends BaseController
 {
-	public function registerPage()
-	{
-		$request = request();
-		if ($request->is('get')) return view('register_page');
+  public function registerPage()
+  {
+    $request = request();
+    if ($request->is('get')) return view('register_page');
 
-		$username = $request->getPost('username');
-		$email =  $request->getPost('email');
-		$phonenumber =  $request->getVar('phonenumber');
-		$password = $request->getVar('password');
-		$gender = $request->getVar('gender');
+    $username = $request->getPost('username');
+    $email =  $request->getPost('email');
+    $fullname =  $request->getPost('fullname');
+    $phonenumber =  $request->getVar('phonenumber');
+    $password = $request->getVar('password');
+    $gender = $request->getVar('gender');
 
-		$validation = \Config\Services::validation();
+    $validation = \Config\Services::validation();
 
-		$rules = [
-			'username' => 'required|max_length[255]',
-			'phonenumber' => 'required|max_length[15]',
-			'password' => 'required|max_length[255]|min_length[8]',
-			'gender' => 'required|max_length[1]',
-		];
+    $rules = [
+      'username' => 'required|max_length[255]',
+      'phonenumber' => 'required|max_length[15]',
+      'password' => 'required|max_length[255]|min_length[8]',
+      'gender' => 'required',
+    ];
+    $validation->setRules($rules);
 
-		$validation->setRules($rules);
+    // dd($gender);
 
-		if ($validation->withRequest($request)->run()) {
-			$userModel = $this->hasUser($username);
+    if ($validation->withRequest($request)->run()) {
+      $userModel = $this->hasUser($username);
 
-			if ($userModel) {
-				session()->setFlashdata('error', 'Username ' . $username . ' sudah dipakai orang lain');
-			} else {
-				$model = new UsersModel();
-				// dd([$phonenumber, intval($phonenumber)]);
-				$user_id = $model->insert([
-					'username' => $username,
-					'email' => $email,
-					'password' => password_hash($password, PASSWORD_DEFAULT),
-					'fullname' => $username . ' ' . $email,
-					'gender' => $gender,
-					'phone_number' => $phonenumber,
-				]);
+      if ($userModel) {
+        session()->setFlashdata('error', 'Username ' . $username . ' sudah dipakai orang lain');
+      } else {
+        $model = new UsersModel();
+        // dd([$phonenumber, intval($phonenumber)]);
+        $user_id = $model->insert([
+          'username' => $username,
+          'email' => $email,
+          'password' => password_hash($password, PASSWORD_DEFAULT),
+          'fullname' => $fullname,
+          'gender' => $gender,
+          'phone_number' => $phonenumber,
+        ]);
 
-				session()->set([
-					'user_id' => $user_id,
-					'logged_in' => true,
-				]);
+        session()->set([
+          'user_id' => $user_id,
+          'logged_in' => true,
+        ]);
 
-				return redirect()->to(base_url());
-			}
-		} else {
-			session()->setFlashdata('error', 'Data yang diinputkan tidak valid');
-			// dd($validation->listErrors());
-		}
+        return redirect()->to(base_url());
+      }
+    } else {
+      session()->setFlashdata('error', 'Data yang diinputkan tidak valid');
+      dd($validation->listErrors());
+    }
 
-		return redirect()->to(base_url('register'));
-	}
+    session()->setFlashdata('current_input', [
+      'username' => $username,
+      'email' => $email,
+      'fullname' => $fullname,
+      'gender' => $gender,
+      'phone_number' => $phonenumber,
+    ]);
+    return redirect()->to(base_url('register'));
+  }
 
-	public function loginPage()
-	{
-		$request = request();
-		if ($request->is('get')) return view('login_page');
+  public function loginPage()
+  {
+    $request = request();
+    if ($request->is('get')) return view('login_page');
 
-		$username = $request->getPost('username');
-		$password = $request->getVar('password');
+    $username = $request->getPost('username');
+    $password = $request->getVar('password');
 
-		$validation = \Config\Services::validation();
+    $validation = \Config\Services::validation();
 
-		$rules = [
-			'username' => 'required|max_length[255]',
-			'password' => 'required|max_length[255]|min_length[8]',
-		];
+    $rules = [
+      'username' => 'required|max_length[255]',
+      'password' => 'required|max_length[255]|min_length[8]',
+    ];
 
-		$validation->setRules($rules);
+    $validation->setRules($rules);
 
-		if ($validation->withRequest($request)->run()) {
-			$userModel = $this->hasUser($username);
+    if ($validation->withRequest($request)->run()) {
+      $userModel = $this->hasUser($username);
 
-			if ($userModel && password_verify($password, $userModel['password'])) {
-				session()->set([
-					'user_id' => $userModel['user_id'],
-					'logged_in' => true,
-				]);
-				return redirect()->to(base_url());
-			} else {
-				session()->setFlashdata('error', 'Username dan password salah');
-			}
-		} else {
-			session()->setFlashdata('error', 'Data yang diinputkan tidak valid');
-			// dd($validation->getValidated());
-		}
+      if ($userModel && password_verify($password, $userModel['password'])) {
+        session()->set([
+          'user_id' => $userModel['user_id'],
+          'logged_in' => true,
+        ]);
+        return redirect()->to(base_url());
+      } else {
+        session()->setFlashdata('error', 'Username dan password salah');
+      }
+    } else {
+      session()->setFlashdata('error', 'Data yang diinputkan tidak valid');
+      // dd($validation->getValidated());
+    }
 
-		return redirect()->to(base_url('login'));
-	}
+    session()->setFlashdata('current_input', [
+      'username' => $username
+    ]);
+    return redirect()->to(base_url('login'));
+  }
 
-	public function logout()
-	{
-		session()->destroy();
-		return redirect()->to(base_url());
-	}
+  public function logout()
+  {
+    session()->destroy();
+    return redirect()->to(base_url());
+  }
 
-	private function hasUser($username)
-	{
-		$model = new UsersModel();
-		$user = $model->where(['username' => $username]);
+  private function hasUser($username)
+  {
+    $model = new UsersModel();
+    $user = $model->where(['username' => $username]);
 
-		return $user->first();
-	}
+    return $user->first();
+  }
 }
--- app/Views/register_page.php
+++ app/Views/register_page.php
@@ -12,8 +12,13 @@
   <form action="" method="post" style="display: flex; flex-direction: column; width: 60%;">
     <input style="padding: 10px 20px;" type="text" placeholder="type your username" name="username" />
     <input style="padding: 10px 20px;" type="email" placeholder="type your email" name="email" />
+    <input style="padding: 10px 20px;" type="text" placeholder="type your Full name" name="fullname" />
     <input style="padding: 10px 20px;" type="text" placeholder="type your phonenumber" name="phonenumber" maxlength="15" max="15" />
     <input style="padding: 10px 20px;" type="text" placeholder="type your password" name="password" />
+
+    <input style="padding: 10px 20px;" type="radio" name="gender" id="l" value="L" /><label for="l">MAN</label>
+    <input style="padding: 10px 20px;" type="radio" name="gender" id="p" value="P" /><label for="p">Women</label>
+
     <input style="padding: 10px 20px;" type="submit" value="submit">
   </form>
 </body>
